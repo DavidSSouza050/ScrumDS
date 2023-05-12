d5fa163a70c67c47696a8e25f2d13d3858308190
Jeremias João Mané <jmane@apoioprodesp.sp.gov.br>
2023-05-12 20:47:11 -0300

    ajustes

--- backend/ssd/ssd/src/main/java/com/ssd/ssd/utils/Encrypt.java
+++ backend/ssd/ssd/src/main/java/com/ssd/ssd/utils/Encrypt.java
@@ -10,10 +10,10 @@ public class Encrypt {
 	
 Encrypt(){}
 	
-	public static String getHash(String senha) {
+	public static String getHash(String digits) {
 		try {
 	      MessageDigest md = MessageDigest.getInstance("SHA-256");
-	      byte[] digestResult = md.digest(senha.getBytes());
+	      byte[] digestResult = md.digest(digits.getBytes());
 	      return Base64.getEncoder().encodeToString(digestResult);
 		}catch(NoSuchAlgorithmException ex) {
 			throw new MsgException("Erro ao criptografar senha!");
